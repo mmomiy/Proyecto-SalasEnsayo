a57e84fd7178154b8a0f9db5e1d40c240f6fa1b8
Francesco Rodríguez <lrodriguezsanc@gmail.com>
2012-03-19 09:30:02 -0500

    Fix indentation on user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ActiveRecord::Base
-validates_presence_of :uid, :provider
-validates_uniqueness_of :uid, :scope => :provider
+  validates_presence_of :uid, :provider
+  validates_uniqueness_of :uid, :scope => :provider
 
-def self.create_with_omniauth(auth)  
-    create! do |user|  
-      user.provider = auth["provider"]  
-      user.uid = auth["uid"] 
+  def self.create_with_omniauth(auth)
+    create! do |user|
+      user.provider = auth["provider"]
+      user.uid = auth["uid"]
       user.name = auth["info"]["name"]
-    end  
-end  
+    end
+  end
 end
